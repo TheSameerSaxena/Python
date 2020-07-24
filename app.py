a841e9f84e515272e360b50efba5863e80a40f54
TheSameerSaxena <50220880+TheSameerSaxena@users.noreply.github.com>
2020-07-24 10:16:44 +0530

    Weight Converter
    Python program to convert lbs into kgs and vice versa

---
+++ app.py
@@ -0,0 +1,10 @@
+weight = int(input("Enter your weight: "))
+choice = input("(L)bs or (K)gs ? ")
+if choice == 'l' or choice == 'L':
+    wei = str((weight*0.45))
+    print("Your weight in Kilos is: "+wei+" Kgs")
+elif choice == 'k' or choice == 'K':
+    wei1 = str((weight*2.205))
+    print("Your weight in Pounds is: "+wei1+" Pounds")
+else:
+    print("Wrong Choice.")
