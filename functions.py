9844b124cc18ebc05c4a7713f1644c594bd47e93
TheSameerSaxena <50220880+TheSameerSaxena@users.noreply.github.com>
2020-08-01 22:43:03 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def computepay(h, r):
+    if h > 40:
+        p = 1.5 * r * (h - 40) + (40 * r)
+    else:
+        p = h * r
+    return p
+
+
+hrs = float(input("Enter Hours:"))
+rphrs = float(input("Enter rate per hour:"))
+
+p = computepay(hrs, rphrs)
+print('Pay ' + str(p))
