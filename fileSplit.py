25efb30c8473362c6d07ca1c5f5a3a4311a2ea8b
TheSameerSaxena <50220880+TheSameerSaxena@users.noreply.github.com>
2020-08-21 03:12:19 +0530

    Files
    Manipulating file using split(), rstrip().

--- fileSplit.py
+++ fileSplit.py
@@ -1,3 +1,11 @@
+"""Open the file mbox-short.txt and read it line by line.
+
+When you find a line that starts with 'From ' like the following line:
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message).
+Then print out a count at the end."""
+
 fhand = open('mbox-short.txt')
 count = 0
 for line in fhand:
@@ -7,4 +15,4 @@ for line in fhand:
     mail = words[1]
     count += 1
     print(mail)
-print('There were',count, 'lines in the file with From as the first word')
+print('There were',count, 'lines in the file with From as the first word') 
